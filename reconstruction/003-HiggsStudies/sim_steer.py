26134477564f96e92a9cecbaf4b1d062ebb17ba6
sslomte <86433874+sslomte@users.noreply.github.com>
2022-04-07 19:24:42 -0500

    1evt

--- reconstruction/003-HiggsStudies/sim_steer.py
+++ reconstruction/003-HiggsStudies/sim_steer.py
@@ -20,7 +20,7 @@ SIM.hepmc3.useHepMC3 = False
 ## Macro file to execute for runType 'run' or 'vis'
 SIM.macroFile = []
 ## number of events to simulate, used in batch mode. -1 all
-SIM.numberOfEvents = 20
+SIM.numberOfEvents = 1
 ## Outputfile from the simulation,only lcio output is supported
 SIM.outputFile = "mumu_H_bb.slcio"
 ## Verbosity use integers from 1(most) to 7(least) verbose
